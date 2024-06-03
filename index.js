02811bb1d00a7e897b10ebb1394d071a7c89f5a1
EldiviX <danya.asanov1@gmail.com>
2024-06-03 15:35:26 +0300

    .

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { UserController, AdController } from "./controllers/index.js";
 import checkAuth from "./utils/checkAuth.js";
 import handleValidationErrors from "./utils/handleValidationErrors.js";
 
-mongoose.connect("mongodb://0.0.0.0:27017/test")
+mongoose.connect("mongodb://localhost:27017/test")
     .then(() => console.log('DB Connected'))
     .catch((error) => console.log('Error', error))
 

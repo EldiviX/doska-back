7ec6ed42022a69f3a40f80fd75464bb606fd4477
ViJi <danya.asanov1@gmail.com>
2024-06-05 22:37:48 +0300

    /

--- index.js
+++ index.js
@@ -36,21 +36,11 @@ const upload = multer({ storage });
 
 app.use(express.json());
 app.use(cors());
+app.options('*', cors());
 
 app.use('/uploads', express.static('uploads'));
 
-app.post('/login', loginValidation, handleValidationErrors, (req, res) => {
-    // Логика обработки запроса
-    console.log('dasdasdas');
-    // Установка заголовков CORS
-    res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    // Отправка ответа
-    res.send(/* Ваш ответ */);
-});
-
+app.post('/login', cors(), loginValidation, handleValidationErrors, UserController.login);
 app.post('/register', registerValidation, handleValidationErrors, UserController.register);
 
 app.get('/me', checkAuth, UserController.getMe);
